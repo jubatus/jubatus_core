c5e130aa98e6f3076e6c1dc4ae134a73bc014f6d
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2014-06-13 16:33:06 +0900

    remove glog dependency from assertion macro

--- jubatus/core/common/assert.hpp
+++ jubatus/core/common/assert.hpp
@@ -19,36 +19,43 @@
 
 #ifndef JUBATUS_DISABLE_ASSERTIONS
 
-#include <glog/logging.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <iostream>
+
+#define CHECK(expr, message) { \
+    if (!(expr)) { \
+      std::cerr << "ASSERTION FAILED: " << message << ": " \
+                << #expr << std::endl; \
+      abort(); \
+    }}
 
 // declares expr to be true
 // unlike glog CHECK, this macro cannot take trailing `<<';
 // please use JUBATUS_ASSERT_MSG if you need extra messages.
-#define JUBATUS_ASSERT(expr) do { CHECK(expr); } while (0)
+#define JUBATUS_ASSERT(expr) do { CHECK((expr), ""); } while (0)
 
-// declares expr to be true; if false, messages are shown after #expr
-// messages are separated by `<<', such as
-//   JUBATUS_ASSERT_MSG(a == b, ": a=" << a << ", b=" << b);
+// declares expr to be true; if false, messages are shown
 #define JUBATUS_ASSERT_MSG(expr, messages)  \
-    do { CHECK(expr) << messages; } while (0)
+    do { CHECK((expr), messages); } while (0)
 
 // declares control flow not to reach here
 #define JUBATUS_ASSERT_UNREACHABLE()   \
-    do { LOG(FATAL) << "control flow should not reach here"; } while (0)
+    do { CHECK(0, "control flow not to reach here"); } while (0)
 
-// helpers to compare values (if assertion failed, values are shown)
+// helpers to compare values
 #define JUBATUS_ASSERT_EQ(a, b, messages)   \
-    do { CHECK_EQ(a, b) << messages; } while (0)
+    do { CHECK((a) == (b), messages); } while (0)
 #define JUBATUS_ASSERT_NE(a, b, messages)   \
-    do { CHECK_NE(a, b) << messages; } while (0)
+    do { CHECK((a) != (b), messages); } while (0)
 #define JUBATUS_ASSERT_LE(a, b, messages)   \
-    do { CHECK_LE(a, b) << messages; } while (0)
+    do { CHECK((a) <= (b), messages); } while (0)
 #define JUBATUS_ASSERT_LT(a, b, messages)   \
-    do { CHECK_LT(a, b) << messages; } while (0)
+    do { CHECK((a) < (b), messages);  } while (0)
 #define JUBATUS_ASSERT_GE(a, b, messages)   \
-    do { CHECK_GE(a, b) << messages; } while (0)
+    do { CHECK((a) >= (b), messages); } while (0)
 #define JUBATUS_ASSERT_GT(a, b, messages)   \
-    do { CHECK_GT(a, b) << messages; } while (0)
+    do { CHECK((a) > (b), messages);  } while (0)
 
 #else  // #ifndef JUBATUS_DISABLE_ASSERTIONS
 
