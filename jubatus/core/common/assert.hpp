f6d385762d3149f998b375e07858079727559a22
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2014-06-17 15:48:10 +0900

    replace CHECK with JUBA_CHECK

--- jubatus/core/common/assert.hpp
+++ jubatus/core/common/assert.hpp
@@ -23,7 +23,7 @@
 #include <assert.h>
 #include <iostream>
 
-#define CHECK(expr, message) { \
+#define JUBA_CHECK(expr, message) { \
     if (!(expr)) { \
       std::cerr << "ASSERTION FAILED: " << message << ": " \
                 << #expr << std::endl; \
@@ -33,29 +33,29 @@
 // declares expr to be true
 // unlike glog CHECK, this macro cannot take trailing `<<';
 // please use JUBATUS_ASSERT_MSG if you need extra messages.
-#define JUBATUS_ASSERT(expr) do { CHECK(expr, ""); } while (0)
+#define JUBATUS_ASSERT(expr) do { JUBA_CHECK(expr, ""); } while (0)
 
 // declares expr to be true; if false, messages are shown
 #define JUBATUS_ASSERT_MSG(expr, messages)  \
-    do { CHECK(expr, messages); } while (0)
+    do { JUBA_CHECK(expr, messages); } while (0)
 
 // declares control flow not to reach here
 #define JUBATUS_ASSERT_UNREACHABLE()   \
-    do { CHECK(0, "control flow not to reach here"); } while (0)
+    do { JUBA_CHECK(0, "control flow not to reach here"); } while (0)
 
 // helpers to compare values
 #define JUBATUS_ASSERT_EQ(a, b, messages)   \
-    do { CHECK((a) == (b), messages); } while (0)
+    do { JUBA_CHECK((a) == (b), messages); } while (0)
 #define JUBATUS_ASSERT_NE(a, b, messages)   \
-    do { CHECK((a) != (b), messages); } while (0)
+    do { JUBA_CHECK((a) != (b), messages); } while (0)
 #define JUBATUS_ASSERT_LE(a, b, messages)   \
-    do { CHECK((a) <= (b), messages); } while (0)
+    do { JUBA_CHECK((a) <= (b), messages); } while (0)
 #define JUBATUS_ASSERT_LT(a, b, messages)   \
-    do { CHECK((a) < (b), messages);  } while (0)
+    do { JUBA_CHECK((a) < (b), messages);  } while (0)
 #define JUBATUS_ASSERT_GE(a, b, messages)   \
-    do { CHECK((a) >= (b), messages); } while (0)
+    do { JUBA_CHECK((a) >= (b), messages); } while (0)
 #define JUBATUS_ASSERT_GT(a, b, messages)   \
-    do { CHECK((a) > (b), messages);  } while (0)
+    do { JUBA_CHECK((a) > (b), messages);  } while (0)
 
 #else  // #ifndef JUBATUS_DISABLE_ASSERTIONS
 
