758aef21b4f7b1600759ea15b7c2108ca75f7893
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2014-10-17 18:09:50 +0900

    Split select_by_weights

--- jubatus/core/bandit/select_by_weights.cpp
+++ jubatus/core/bandit/select_by_weights.cpp
@@ -14,45 +14,30 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-#include "softmax.hpp"
-
-#include <string>
-#include <vector>
-#include <cmath>
-#include <numeric>
-#include "../common/exception.hpp"
 #include "select_by_weights.hpp"
+
+#include <vector>
+#include <numeric>
+
+using jubatus::util::math::random::mtrand;
 
 namespace jubatus {
 namespace core {
 namespace bandit {
 
-softmax::softmax(
-    const jubatus::util::lang::shared_ptr<storage>& s,
-    double tau)
-    : bandit_base(s), tau_(tau) {
-  if (tau <= 0) {
-    throw JUBATUS_EXCEPTION(
-        common::invalid_parameter("0 < tau"));
-  }
-}
+int select_by_weights(
+    const std::vector<double>& weights, mtrand& rand) {
+  double total_weight = std::accumulate(weights.begin(), weights.end(), 0.0);
+  double x = rand.next_double(total_weight);
 
-std::string softmax::select_arm(const std::string& player_id) {
-  const std::vector<std::string>& arms = get_arms();
-  if (arms.empty()) {
-    throw JUBATUS_EXCEPTION(
-        common::exception::runtime_error("arm is not registered"));
+  double w = 0;
+  for (size_t i = 0; i < weights.size() - 1; ++i) {
+    w += weights[i];
+    if (x < w) {
+      return i;
+    }
   }
-
-  const storage& s = get_storage();
-  std::vector<double> weights;
-  weights.reserve(arms.size());
-
-  for (size_t i = 0; i < arms.size(); ++i) {
-    double expectation = s.get_expectation(player_id, arms[i]);
-    weights.push_back(std::exp(expectation / tau_));
-  }
-  return arms[select_by_weights(weights, rand_)];
+  return weights.size() - 1;
 }
 
 }  // namespace bandit
