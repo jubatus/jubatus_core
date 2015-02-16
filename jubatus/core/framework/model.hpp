5e41e9d5b018fce47d178ef761e22d13509ce91e
Gentaro Watanabe <g.wtnb@preferred.jp>
2015-02-16 16:41:35 +0900

    Replace "Preferred Infrastructure" with "Preferred Networks" in copyright notice.

--- jubatus/core/framework/model.hpp
+++ jubatus/core/framework/model.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2014 Preferred Infrastructure and Nippon Telegraph and Telephone Corporation.
+// Copyright (C) 2014 Preferred Networks and Nippon Telegraph and Telephone Corporation.
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
