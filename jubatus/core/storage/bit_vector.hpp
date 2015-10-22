ee93ba5fabdda97bcc77c51c283c39da2eb44b5d
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2015-10-22 13:34:22 +0900

    fix ouf-of-bound access to bits_ when vector length is 0

--- jubatus/core/storage/bit_vector.hpp
+++ jubatus/core/storage/bit_vector.hpp
@@ -284,7 +284,7 @@ struct bit_vector_base {
     if (bits_ == NULL) {
       return true;
     }
-    for (int64_t i = (used_bytes() / sizeof(bit_base)) - 1; i >= 0; --i) {
+    for (size_t i = 0, blocks = used_bytes() / sizeof(bit_base); i < blocks; ++i) {
       if (bits_[i] != 0) {
         return false;
       }
@@ -318,7 +318,7 @@ struct bit_vector_base {
       return bit_count();
     }
     size_t match_num = 0;
-    for (size_t i = 0; i < used_bytes() / sizeof(bit_base); ++i) {
+    for (size_t i = 0, blocks = used_bytes() / sizeof(bit_base); i < blocks; ++i) {
       match_num += detail::bitcount(bits_[i] ^ bv.bits_[i]);
     }
     return match_num;
@@ -328,7 +328,7 @@ struct bit_vector_base {
       return 0;
     }
     size_t result = 0;
-    for (int64_t i = (used_bytes() / sizeof(bit_base)) - 1; i >= 0; --i) {
+    for (size_t i = 0, blocks = used_bytes() / sizeof(bit_base); i < blocks; ++i) {
       result += detail::bitcount(bits_[i]);
     }
     return result;
