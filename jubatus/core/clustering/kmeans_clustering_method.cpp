0d0b04e4aaeb6b03da25d2a38fe291d4ce105b2f
IMAMASU Ryohei <imms.ryohei@gmail.com>
2015-11-24 21:08:44 +0900

    Fix kmeans to throw exception in get_k_center when clustering is not performed

--- jubatus/core/clustering/kmeans_clustering_method.cpp
+++ jubatus/core/clustering/kmeans_clustering_method.cpp
@@ -98,6 +98,11 @@ void kmeans_clustering_method::online_update(wplist points) {
 }
 
 vector<common::sfv_t> kmeans_clustering_method::get_k_center() const {
+  if (kcenters_.empty()) {
+    throw JUBATUS_EXCEPTION(common::exception::runtime_error(
+        "clustering is not performed yet"));
+  }
+
   return kcenters_;
 }
 
