32c00192f05cc9d1028e086be7000fc30293a268
IMAMASU Ryohei <imms.ryohei@gmail.com>
2017-02-22 21:31:52 +0900

    Fix double acquision of a lock in column_table::delete_row

--- jubatus/core/storage/column_table.hpp
+++ jubatus/core/storage/column_table.hpp
@@ -373,7 +373,7 @@ class column_table {
   }
 
   bool delete_row_nolock(uint64_t index) {
-    if (size() <= index) {
+    if (size_nolock() <= index) {
       return false;
     }
     delete_row_(index);
@@ -406,7 +406,7 @@ class column_table {
   index_table index_;
 
   void delete_row_(uint64_t index) {
-    JUBATUS_ASSERT_LT(index, size(), "");
+    JUBATUS_ASSERT_LT(index, size_nolock(), "");
 
     for (std::vector<detail::abstract_column>::iterator jt = columns_.begin();
          jt != columns_.end();
